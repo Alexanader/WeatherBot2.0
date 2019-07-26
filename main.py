4dd378f10b34634ddfb3acdfcf25487e2f064bb2
Alexanader <berestok6797@gmail.com>
2019-07-26 16:32:48 +0300

    fcom111as2w1wwsd

--- main.py
+++ main.py
@@ -301,8 +301,8 @@ def webhook():
 
             
 if __name__ == '__main__':
-    host="0.0.0.0"
-    port=int(os.environ.get('PORT', 5000))
+    #host="0.0.0.0"
+    port=int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
     #server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
     # bot.polling(none_stop=True, )
